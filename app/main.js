99b84565569cc0ebf3784d0f6c35e1be26cf5f75
Queren Hapuque <queren.fernandes@yahoo.com>
2023-03-10 17:28:36 -0300

    começando com o foreach

--- app/main.js
+++ app/main.js
@@ -5,5 +5,7 @@ const endpointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.js
 getBuscarLivrosDaAPI()
 
 async function getBuscarLivrosDaAPI() {
-    const res = await fetch(endpointDaAPI)
+    const res = await fetch(endpointDaAPI);
+    livros = await res.json();
+    console.table(livros)
 }
